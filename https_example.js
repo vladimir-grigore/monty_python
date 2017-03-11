3bbe6ed3fe9f8a591e890b114081161c320cf68e
vagrant <vladimir.grigore@gmail.com>
2017-03-11 22:25:24 +0000

    Create a simple https request

--- https_example.js
+++ https_example.js
@@ -1,1 +1,17 @@
 var https = require('https');
+
+var options = {
+  host: "www.example.com",
+  path: '/'
+};
+
+// called by https when the request is made.
+var callbackFunction = function(){
+  console.log("In response handler callback method.");
+};
+
+console.log("I'm about to make the request.");
+
+https.request(options, callbackFunction).end();
+
+console.log("I've made the request.");
